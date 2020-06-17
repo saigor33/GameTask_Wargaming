2ce361f7a6d403646eccccd256cd23f42a3b5b7e
saigor33 <52320952+saigor33@users.noreply.github.com>
2020-06-17 16:46:41 +0300

    Создано управление кораблём

--- Assets/Prefabs/ShipMovementLogical.cs
+++ Assets/Prefabs/ShipMovementLogical.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class ShipMovementLogical : MonoBehaviour
 {
+    [SerializeField] private float _speedMoving;
+    [SerializeField] private float _speedRotate;
+
+
+    private void FixedUpdate()
+    {
+        if (!Input.anyKey)
+            return;
+
+       // MovementLogical();
+
+    }
+
     private void Update()
     {
-        
+        if (!Input.anyKey)
+            return;
+        MovementLogical();
+        CheckLaunchPlan();
+    }
+
+    private void MovementLogical()
+    {
+
+        float shipMovingDirect = Input.GetAxis("ShipMoving");
+        if (shipMovingDirect !=0)
+        {
+            Vector3 movementDirection = new Vector3(0, shipMovingDirect, 0);
+            transform.Translate(movementDirection * _speedMoving * Time.deltaTime);
+        }
+
+        float shipRotateDirect = Input.GetAxis("ShipRotation");
+        if (shipRotateDirect != 0)
+        {
+            Vector3 rotationDirect = new Vector3(0, 0, shipRotateDirect);
+            transform.Rotate(rotationDirect * _speedRotate * Time.deltaTime);
+        }
+    }
+
+    private void CheckLaunchPlan()
+    {
+        if (Input.GetKeyDown("LaunchPlane"))
+        {
+
+        }
     }
 }
